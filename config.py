3eb355e1d857fcef03880b1def7a082eb49475df
Brian Patrick van Oers <brianpvanoers@protonmail.com>
2025-10-18 18:59:25 +0200

    Update config file structure

--- config.py
+++ config.py
@@ -17,21 +17,23 @@
 * loop_until_complete only works when: retry_failed_experiments = True and ignore_existing_files = False
 """
 
-# Data preparation
+# -- Settings ---------------------------------------------------------------------------------------------------------
+
+# Data preparation settings
 dataset = ['health', 'fashion_mnist']  # Options: ['spam', 'letter', 'health', 'mnist', 'fashion_mnist', 'cifar10']
 miss_rate = [0.2]
 miss_modality = ['MCAR']               # Options: ['MCAR', 'MAR', 'MNAR']
 seed = [0]                             # Use None for random seed
 store_prepared_dataset = True          # Default: True
 
-# S-GAIN
+# S-GAIN settings
 version = ['TFv1_FP32']  # Options: ['TFv1_FP32', 'TFv1_INT8']
 batch_size = [128]       # Default: [128]
 hint_rate = [0.9]        # Default: [0.9]
 alpha = [100]            # Default: [100]
 iterations = [10000]     # Default: [10000]
 
-# Generator
+# Generator settings
 generator_sparsity = [0, 0.6, 0.8, 0.9, 0.95, 0.99]
 generator_initialization = ['dense', 'random']       # Options: ['dense', 'random', 'ER', 'ERRW']
 generator_regrower = [None]  # Todo list options
@@ -55,6 +57,13 @@ discriminator_prune_period = [None]
 discriminator_enable_clipping = [False]
 discriminator_use_strategy = [False]
 
+# Output settings
+output_folder = 'output'  # Default: 'output'
+no_imputation = False     # Default: False
+no_log = False            # Default: False (also disables graphs)
+no_graphs = False         # Default: False
+no_model = False          # Default: False
+
 # Monitor settings
 enable_rmse_monitor = True                 # Default: True
 enable_imputation_time_monitor = True      # Default: True
@@ -64,23 +73,6 @@ enable_sparsity_monitor = True             # Default: True
 enable_FLOPs_monitor = False               # Default: False (takes significantly more time)
 enable_loss_monitor = True                 # Default: True
 
-# Output settings
-output_folder = 'output'       # Default: 'output'
-verbose = True                 # Default: True
-no_imputation = False          # Default: False
-no_log = False                 # Default: False (also disables graphs)
-no_graphs = False              # Default: False
-no_model = False               # Default: False
-no_system_information = False  # Default: False
-
-# Run settings
-n_runs = 10
-ignore_existing_files = False    # Default: False
-retry_failed_experiments = True  # Default: True
-loop_until_complete = True       # Default: True
-perform_analysis = True          # Default: True
-auto_shutdown = True             # Default: False
-
 # Analysis settings
 analysis_folder = 'analysis'     # Default: 'analysis'
 compile_metrics = True           # Default: True
@@ -89,6 +81,14 @@ plot_success_rate = True         # Default: True
 plot_imputation_time = True      # Default: True
 plot_memory_usage = False        # Default: True
 plot_energy_consumption = False  # Default: True
+
+# Run settings
+n_runs = 10
+ignore_existing_files = False    # Default: False
+retry_failed_experiments = True  # Default: True
+loop_until_complete = True       # Default: True
+perform_analysis = True          # Default: True
+auto_shutdown = True             # Default: False
 
 # Inclusions (modify the settings and run again)
 inclusions = [{
@@ -100,3 +100,7 @@ inclusions = [{
 
 # Exclusions (overwrites inclusions)
 exclusions = []
+
+# Options
+verbose = True                 # Default: True
+no_system_information = False  # Default: False
